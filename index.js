a94cfa802b498116610250064978e95b7720220c
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-14 15:58:48 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (aname) =>
+      aname === name ||
+      aname === name.toLowerCase() ||
+      aname === name.toUpperCase(),
+  );
+}
+
+function fuzzyMatch(drivers, firstNameLetters) {
+  let i = firstNameLetters.length;
+  return drivers.filter((name) => name.slice(0, i) === firstNameLetters);
+}
+
+
+function matchName(drivers, name) {
+  return drivers.filter((obj) => obj.name === name);
+}
